7bc20fa8deca8d6120e325c24d7dd965e14acf94
Lukasz Kolodzinski <luk.kolodzinski@gmail.com>
2018-07-23 19:14:59 +0200

    Created: blog_test.py with test_create_bog

---
+++ blog/tests/unit/blog_test.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+from blog.blog import Blog
+
+class BlogTest(TestCase):
+    def test_create_blog(self):
+        new_blog = Blog("Ian Kowalsky", "Cookies Time")
+
+        self.assertEqual("Ian Kowalsky", new_blog.author)
+        self.assertEqual("Cookies Time", new_blog.title)
+
