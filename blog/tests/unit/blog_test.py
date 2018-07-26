5e6a03ca9815e6b3df62ef96c9c05d768e453f34
Lukasz <kolodzinski.lukasz@gmail.com>
2018-07-26 14:45:31 +0200

    Created: test_repr_none_post

--- blog/tests/unit/blog_test.py
+++ blog/tests/unit/blog_test.py
@@ -9,5 +9,10 @@ class BlogTest(TestCase):
         self.assertEqual("Cookies Time", new_blog.title)
     #   self.assertListEqual([], new_blog.posts)
         self.assertEqual(0, len(new_blog.posts))
-        
+
+    def test_repr_none_post(self):
+        single_post_blog = Blog("Ian Kowalsky", "Cookies Time")
+
+        self.assertEqual('Cookies Time by Ian Kowalsky (0 posts available)',
+                         single_post_blog.__repr__())
 
