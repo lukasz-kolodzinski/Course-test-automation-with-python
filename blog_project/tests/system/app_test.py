a15e7beed400c61023b2e2e8141a0e8ec46ad707
Lukasz <kolodzinski.lukasz@gmail.com>
2018-08-06 09:44:08 +0200

    Crated: test_menu_user_input

--- blog_project/tests/system/app_test.py
+++ blog_project/tests/system/app_test.py
@@ -11,4 +11,9 @@ class AppTest(TestCase):
             blog_project.app.print_blogs()
             mocked_print.assert_called_with('Test by Test Author (0 posts available)')
 
+    def test_menu_user_input(self):
+        with patch('builtins.input') as mocked_input:
+            blog_project.app.menu()
+            #mocked_input.assert_called_with('Enter "c" to create new blog; "l" to list blogs; "r" to read one, "p" to create post; "q" to quit')
+            mocked_input.assert_called_with(blog_project.app.MENU_PROMPT)
 
