3af4fb7ea0724f90ef3fcdff4efc574d2440b1d0
Lukasz <kolodzinski.lukasz@gmail.com>
2018-08-08 11:21:34 +0200

    Added: selection loop into menu function

--- blog_project/app.py
+++ blog_project/app.py
@@ -4,9 +4,15 @@ blogs = dict()
 def menu():
     print_blogs ()
     selection = input(MENU_PROMPT)
-
+    while selection != 'q':
+        if selection == 'c':
+            ask_create_blog()
+        elif selection == 'l':
+            print_blogs()
+        elif selection == 'r':
+            ask_read_blog()
+        selection =input(MENU_PROMPT)
 
 def print_blogs():
     for key, blog in blogs.items():
         print("{}".format(blog))
-
