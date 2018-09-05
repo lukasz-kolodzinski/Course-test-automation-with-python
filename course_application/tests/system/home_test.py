c9ad742e73150ed5704980891820ea709e71f1d2
Lukasz <kolodzinski.lukasz@gmail.com>
2018-09-05 14:59:00 +0200

    Changed: fixed test_home

--- course_application/tests/system/home_test.py
+++ course_application/tests/system/home_test.py
@@ -3,13 +3,13 @@ import json
 
 class TestHome (BaseTest):
     def test_home(self):
-        with self.app() as homesite:
+        with self.app as homesite:
             response = homesite.get("/")
 
             self.assertEqual(response.status_code, 200)
 
     def test_home_response_json(self):
-        with app.test_client() as homesite:
+        with self.app as homesite:
             response = homesite.get("/")
 
             self.assertEqual(json.loads(response.get_data()),
