f70d4b3cf6383583bc04de9f05682c4985f00209
Lukasz <kolodzinski.lukasz@gmail.com>
2018-09-05 13:30:12 +0200

    Added: test_home_response_json

--- course_application/tests/system/home_test.py
+++ course_application/tests/system/home_test.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from course_application.app import app
+import json
 
 class TestHome (TestCase):
     def test_home(self):
@@ -7,3 +8,10 @@ class TestHome (TestCase):
             response = homesite.get("/")
 
             self.assertEqual(response.status_code, 200)
+
+    def test_home_response_json(self):
+        with app.test_client() as homesite:
+            response = homesite.get("/")
+
+            self.assertEqual(json.loads(response.get_data()),
+                             {'message': 'Hello world'})
