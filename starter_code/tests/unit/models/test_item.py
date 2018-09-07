b3e95768d9a2c80b624a1c532ceae233ce6cbdf4
Lukasz <kolodzinski.lukasz@gmail.com>
2018-09-07 13:27:17 +0200

    Added: initialized ItemTest class with test methods inside

---
+++ starter_code/tests/unit/models/test_item.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+from starter_code.models.item import ItemModel
+
+class ItemTest(TestCase):
+    def test_create_item(self):
+        pass
+
+    def test_create_json(self):
+        pass
