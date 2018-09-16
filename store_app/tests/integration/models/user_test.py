7b4d8dc984725ff1ca1cdfccc0de0ffce12b5a24
Lukasz Kolodzinski <luk.kolodzinski@gmail.com>
2018-09-16 13:33:03 +0200

    Added: integration test in user_tesy.py

---
+++ store_app/tests/integration/models/user_test.py
@@ -0,0 +1,11 @@
+from store_app.models.user import UserModel
+from store_app.tests.integration.base_test import BaseTest
+
+class UserTest(BaseTest):
+    def test_create_user(self):
+        with self.app_context():
+            user = UserModel('test', 'qwerty')
+            user.save_to_database()
+
+            self.assertIsNotNone(UserModel.find_by_username('test'))
+S
