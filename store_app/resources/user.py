ac20a94d2cbfe2b3452cc56ef7f15a6a092f6e51
Lukasz Kolodzinski <luk.kolodzinski@gmail.com>
2018-09-16 12:20:42 +0200

    Added: username and password parsers

--- store_app/resources/user.py
+++ store_app/resources/user.py
@@ -1,3 +1,17 @@
 from flask_restful import Resource, reqparse
 from store_app.models.user import UserModel
 
+class UserRegistration(Resource):
+    """
+        Register user bu sending a POST request with username and password
+    """
+    parser = reqparse.RequestParser()
+    parser.add_argument("username",
+                        type = str,
+                        required = True,
+                        help = "Type string data")
+    parser.add_argument("password",
+                        type = str,
+                        required = True,
+                        help = "This field cannot be blank")
+    
