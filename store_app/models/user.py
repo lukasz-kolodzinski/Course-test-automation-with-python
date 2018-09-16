040851ea4e48ceb79048fbb9e5b2f18f70865203
Lukasz Kolodzinski <luk.kolodzinski@gmail.com>
2018-09-16 12:02:05 +0200

    Added: constructor and classmethods into user.py

--- store_app/models/user.py
+++ store_app/models/user.py
@@ -6,3 +6,20 @@ class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(80))
     password = db.Column(db.String())
+
+    def __init__(self, username, password):
+        self.ussername = username
+        self.password = password
+
+    def save_to_database(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def find_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
+
+    @classmethod
+    def find_by_id(cls, id):
+        return cls.query.filter_by(id=_id).first()
+    
