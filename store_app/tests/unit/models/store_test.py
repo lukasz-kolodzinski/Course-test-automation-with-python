65b6f83c5ea39be56cadb2a8546e5ac3554b8f06
Lukasz Kolodzinski <luk.kolodzinski@gmail.com>
2018-09-16 10:49:34 +0200

    Added: test_create_store

--- store_app/tests/unit/models/store_test.py
+++ store_app/tests/unit/models/store_test.py
@@ -1,3 +1,8 @@
 from store_app.models.store import StoreModel
-from store_app.tests.unit.models import unit_base_test
+from store_app.tests.unit.models.unit_base_test import *
 
+class StoreTest(TestBase):
+    def test_create_store(self):
+        store = StoreModel("test")
+
+        self.assertEqual(store.name, "test")
