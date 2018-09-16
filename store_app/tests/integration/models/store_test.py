c19fdbf4f0b75f8190eb24af8d27847e340ed197
Lukasz Kolodzinski <luk.kolodzinski@gmail.com>
2018-09-16 10:58:03 +0200

    Added: test_create_store_with_no_items

--- store_app/tests/integration/models/store_test.py
+++ store_app/tests/integration/models/store_test.py
@@ -1,3 +1,9 @@
 from store_app.models.store import StoreModel
 from store_app.tests.integration.base_test import BaseTest
 
+class StoreTest(BaseTest):
+    def test_create_store_with_no_items(self):
+        store = StoreModel("test")
+
+        self.assertListEqual(store.items.all(), [])
+        
