1b48735c64a1b29ad1efd2af97dd67736f4c17f6
Lukasz Kolodzinski <luk.kolodzinski@gmail.com>
2018-09-16 11:14:33 +0200

    Added: test_save_item_into_store

--- store_app/tests/integration/models/store_test.py
+++ store_app/tests/integration/models/store_test.py
@@ -1,4 +1,5 @@
 from store_app.models.store import StoreModel
+from store_app.models.item import ItemModel
 from store_app.tests.integration.base_test import BaseTest
 
 class StoreTest(BaseTest):
@@ -13,3 +14,12 @@ class StoreTest(BaseTest):
             store.save_to_db()
 
             self.assertIsNotNone(StoreModel.find_by_name("test"))
+
+    def test_save_item_into_store(self):
+        with self.app_context():
+            store = StoreModel("test")
+            store.save_to_db()
+            item = ItemModel("salt", 3.20, 1)
+            item.save_to_db()
+
+            self.assertEqual(store.items.count(), 1)
